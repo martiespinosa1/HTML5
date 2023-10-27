8158bfe930d4b3da7c75c21c9bf04e828ca8a093
martiespinosa1 <marti.espinosa.7e7@itb.cat>
2023-10-27 11:22:52 +0200

    DOM

---
+++ Exercidis de DOM - JS/script.js
@@ -0,0 +1,21 @@
+// EXERCICI 1
+function estiljs() {
+    document.getElementById("text").style.color = "red";
+}
+
+// EXERCICI 2
+function prenValorForm() {
+    let nom = document.getElementsByTagName("input")[0].value;
+    let cognom = document.getElementsByTagName("input")[1].value;
+
+    console.log("Nom: " + nom);
+    console.log("Cognom: " + cognom);
+}
+
+// EXERCICI 3
+function lorem() {
+    document.getElementsByClassName("lorem")[0].style.backgroundColor = "lightblue";
+    document.getElementsByClassName("lorem")[1].style.backgroundColor = "lightblue";
+}
+
+// EXERCICI 4
