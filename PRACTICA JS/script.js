0987381e9fa5d19059a2a304da22bfff6fecc855
martiespinosa1 <marti.espinosa.7e7@itb.cat>
2023-11-08 10:01:55 +0100

    wip

--- PRACTICA JS/script.js
+++ PRACTICA JS/script.js
@@ -18,51 +18,61 @@ var intents = 0;
 var numerosCorrectes = 0
 
 function comprobar() {
-    numerosCorrectes = 0
-    if (intents < 5) {
-        var numUsuari = document.getElementById("numUsuari").value;
-        numUsuariSplit = numUsuari.split('')
-        for (let i = 0; i < 5; i++) {
-            const newdiv = document.createElement("div");
-            newdiv.className = "newdiv"
-            document.querySelector("main").appendChild(newdiv);
-            newdiv.classList.add("newdiv");
+    if (document.getElementById("numUsuari").value.length === 5) {
+        numerosCorrectes = 0
+        if (intents < 5) {
+            var numUsuari = document.getElementById("numUsuari").value;
+            numUsuariSplit = numUsuari.split('')
+            for (let i = 0; i < 5; i++) {
+                const newdiv = document.createElement("div");
+                newdiv.className = "newdiv"
+                document.querySelector("main").appendChild(newdiv);
+                newdiv.classList.add("newdiv");
 
-            newdiv.innerHTML = numUsuariSplit[i]
+                newdiv.innerHTML = numUsuariSplit[i]
 
-            if (numUsuariSplit[i] === numerorandomSplit[i]) {
-                newdiv.style.backgroundColor = "#00cd00"
-                numerosCorrectes++
-            } else {
-                for (let j = 0; j < 5; j++) {
-                    if (numUsuariSplit[i] === numerorandomSplit[j]) newdiv.style.backgroundColor = "#ffd731"
-                }   
+                if (numUsuariSplit[i] === numerorandomSplit[i]) {
+                    newdiv.style.backgroundColor = "#00cd00"
+                    numerosCorrectes++
+                } else {
+                    for (let j = 0; j < 5; j++) {
+                        if (numUsuariSplit[i] === numerorandomSplit[j]) newdiv.style.backgroundColor = "#ffd731"
+                    }   
+                }
+            }
+            document.getElementById("numUsuari").value = ""
+            intents++
+        
+        } if (numerosCorrectes === 5) {
+            document.getElementById("rojo").innerHTML = "¡¡¡ HAS GANADO !!!" 
+            document.getElementById("rojo").style.backgroundColor = "#00cd00"    
+            intents = 5        
+            
+            const divs = document.querySelectorAll("#arriba div");
+            for (let k = 0; k < divs.length; k++) {
+                divs[k].innerHTML = numerorandomSplit[k];
+            }          
+            document.getElementById("numUsuari").value = ""
+            document.querySelector("button").disabled = true
+
+        } else {
+            switch (intents) {
+                case 1: {
+                    document.getElementById("rojo").innerHTML = "Segundo intento, suerte!" 
+                    break
+                } case 2: {
+                    document.getElementById("rojo").innerHTML = "Tercer intento, suerte!"
+                    break
+                } case 3: {
+                    document.getElementById("rojo").innerHTML = "Cuarto intento, suerte!"
+                    break
+                } case 4: {
+                    document.getElementById("rojo").innerHTML = "Quinto y último intento, surte!"
+                    break
+                } default: document.getElementById("rojo").innerHTML = "HAS PERDIDO"
+                    break
+        
             }
         }
-        document.getElementById("numUsuari").value = ""
-        intents++
-    
-    } if (numerosCorrectes === 5) {
-        document.getElementById("rojo").innerHTML = "Has guanyat!" 
-        document.getElementById("rojo").style.backgroundColor = "#00cd00"    
-        intents = 5
-    } else {
-        switch (intents) {
-            case 1: {
-                document.getElementById("rojo").innerHTML = "Segon intent, sort!" 
-                break
-            } case 2: {
-                document.getElementById("rojo").innerHTML = "Tercer intent, sort!"
-                break
-            } case 3: {
-                document.getElementById("rojo").innerHTML = "Quart intent, sort!"
-                break
-            } case 4: {
-                document.getElementById("rojo").innerHTML = "Últim intent, sort!"
-                break
-            } default: document.getElementById("rojo").innerHTML = "Has perdut"
-    
-        }
-    }
-    
+    } else document.getElementById("rojo").innerHTML = "Alto, tienes que entrar 5 números!"    
 }
